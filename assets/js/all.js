e884de9b281a7330ccdb466c4d1e4a079e98bcb9
Jimmy <gmwuwork@gmail.com>
2020-05-14 11:51:23 +0800

    Update 2020-05-14T03:51:01.360Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,9 @@
-$(function() {
+"use strict";
+
+$(function () {
   /* navbar
   -------------------------------------------------- */
-  $('.js-navbar__toggler').click(function(e) {
+  $('.js-navbar__toggler').click(function (e) {
     e.preventDefault();
     $('.l-navbar__dropdown').toggleClass('active', 200);
   });
@@ -10,8 +12,9 @@ $(function() {
 
   /* fixed navbar addClass topFixedPush
   -------------------------------------------------- */
-  if ( $('nav').is('.js-navbar--topFixed') == true ) {
-    $('body').addClass('js-navbar--topFixedPush')
+
+  if ($('nav').is('.js-navbar--topFixed') == true) {
+    $('body').addClass('js-navbar--topFixedPush');
   }
   /* End of fixed navbar addClass topFixedPush
   -------------------------------------------------- */
@@ -19,29 +22,29 @@ $(function() {
   /* Collapse - accordion
   -------------------------------------------------- */
   // 隱藏所有的 li 內文
-  $('.c-js-accordionBody').hide();
-  // 預設讓第一個 li 加上 active
-  $('.c-js-accordionItem').eq(0).addClass('c-js-accordion--active');
-  // 預設讓第一個 li 內文 顯示
-  $('.c-js-accordionBody').eq(0).show();
-  
-  // 點擊 li
+
+
+  $('.c-js-accordionBody').hide(); // 預設讓第一個 li 加上 active
+
+  $('.c-js-accordionItem').eq(0).addClass('c-js-accordion--active'); // 預設讓第一個 li 內文 顯示
+
+  $('.c-js-accordionBody').eq(0).show(); // 點擊 li
+
   $('.c-js-accordionItem').click(function (e) {
     // 取消預設 event 事件
-    e.preventDefault(); 
-
+    e.preventDefault();
     /**
       *! 加上 active 屬性，並把其他有 active 的移除
       * .siblings() 方法返回被選元素的所有同級元素，同級元素是共享相同父元素的元素。
     */
-    $(this).toggleClass('c-js-accordion--active').siblings().removeClass('c-js-accordion--active');
 
-    // 讓自己的 li body 切換收合
-    $(this).children().next().slideToggle();
+    $(this).toggleClass('c-js-accordion--active').siblings().removeClass('c-js-accordion--active'); // 讓自己的 li body 切換收合
 
-    // 讓其他 li body 收合
+    $(this).children().next().slideToggle(); // 讓其他 li body 收合
+
     $(this).siblings().children().next().slideUp();
-  })
+  });
   /* End of Collapse - accordion
   -------------------------------------------------- */
 });
+//# sourceMappingURL=all.js.map
--- gulpfile.js/options.js
+++
@@ -1,8 +0,0 @@
-var minimist = require('minimist');
-var evnOptions = {
-  string: 'env',
-  default: { env: "develop" }
-}
-var options = minimist(process.argv.slice(2), evnOptions)
-console.log(options);
-exports.options = options;
